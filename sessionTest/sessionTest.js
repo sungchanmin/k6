c372ed0cae81b3023cc049788e94e4c61b183675
chanmin <adorableyou26@gmail.com>
2024-03-18 17:52:49 +0900

    쿠키 추출 테스트 완료
    setup()을 통한 쿠키 추출 후 간편포인트 조회 결과 체크

--- sessionTest/sessionTest.js
+++ sessionTest/sessionTest.js
@@ -9,6 +9,9 @@ export const options = {
   iterations: 1,
 };
 
+/**
+ * setup()은 테스트 수행 전 필요한 요청을 미리 수행합니다. (세션 및 유저 정보 쿠키 추출)
+ */
 export function setup() {
   let FORM_DATA = {
     "loginSearchBean.userId": properties.USER_ID,
@@ -19,28 +22,41 @@ export function setup() {
   };
 
   /**
-   * 로그인 요청시 'loginAction.ez'(302) 응답 후 'main.ez?pc'(200)으로 리다이렉션 된다.
-   * 'res' 변수에는 결과적으로 최종 응답인 'main.ez?pc(200)'이 담긴다.
-   * 'loginAction.ez'(302) 응답이 필요한 경우 옵션으로 '{redirects: 0}'을 명시하여 받을 수 있다.
-   * 'EZWEL_USER_KEY' 쿠키는 'loginAction.ez'(302) 응답 헤더에 담겨있기 때문에 아래와 같이 처리하였다.
+   * 로그인 요청시 'loginAction.ez'(302) 응답 후 'main.ez?pc'(200)으로 리다이렉션 됩니다.
+   * 'res'에는 기본적으로 최종 응답인 'main.ez?pc(200)'이 담기며, 'loginAction.ez'(302) 응답이 필요한 경우 옵션으로 '{redirects: 0}'을 명시하여 받을 수 있습니다.
+   * 최초 응답 헤더에 담겨있는 쿠키를 얻기 위해 아래와 같이 처리하였습니다.
    */
   let res = http.post(properties.LOGIN_ACTION_API, FORM_DATA, { redirects: 0 });
 
-  const SESSION_COOKIE_VALUE = res.cookies.__KSMSID_USER__[0].value;
-  const EZWEL_USER_KEY = res.cookies.EZWEL_USER_KEY[0].value;
-
   return {
-    SESSION_COOKIE_VALUE: SESSION_COOKIE_VALUE,
-    EZWEL_USER_KEY: EZWEL_USER_KEY,
+    SESSION_COOKIE_VALUE: res.cookies.__KSMSID_USER__[0].value,
+    EZWEL_USER_KEY: res.cookies.EZWEL_USER_KEY[0].value,
+    EZWEL_CLIENT_CD: res.cookies.EZWEL_CLIENT_CD[0].value,
   };
 }
 
-export default function (COKKIES) {
-  console.log(COKKIES.SESSION_COOKIE_VALUE);
-  console.log(COKKIES.EZWEL_USER_KEY);
-  // let res = http.get(properties.MAIN_PAGE_URL, {
-  //   cookies: {
-  //     __KSMSID_USER__: SESSION_COOKIE_VALUE,
-  //   },
-  // });
+export default function (COOKIES) {
+
+  // 간편포인트조회 (form 데이터 및 세션 쿠키 포함 POST 요청)
+  let FORM_DATA = {
+    clientCd: COOKIES.EZWEL_CLIENT_CD,
+    userKey: COOKIES.EZWEL_USER_KEY,
+    pageType: "BS",
+    jsonType: "jsonp",
+  };
+
+  let options = {
+    cookies: {
+      __KSMSID_USER__: COOKIES.SESSION_COOKIE_VALUE,
+    },
+  };
+
+  let res = http.post(properties.POINT_CALL_AJAX, FORM_DATA, options);
+  console.log(res.body);
+  
+
+  check(res, { 
+    'HTTP status code check - 간편포인트조회 AJAX status is 200': (r) => r.status == 200, 
+    'Response body JSON data check - userName is 테크부문': (r) => JSON.parse(r.body.substring(1, res.body.length - 1)).userName == '테크부문',
+  });
 }
