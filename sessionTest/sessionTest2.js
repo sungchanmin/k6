403d6837342eff15089917833c4950323d282009
chanmin <adorableyou26@gmail.com>
2024-03-20 14:20:36 +0900

    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성 🎨 테스트 전 쿠키 보관 로직 완료(복지샵) ################ [feat] : 복지샵 로그인 API 요청 및 세션 쿠키 보관 기능 완료, 복지샵 메인 페이지 요청 및 검증 # [fix] : 버그 수정 # [docs] : 문서 수정 # [test] : 테스트 코드 추가 # [refactor] : 코드 리팩토링 # [style] : 코드 의미에 영향을 주지 않는 변경사항 # [chore] : 빌드 부분 혹은 패키지 매니저 수정사항 ################

--- sessionTest/sessionTest2.js
+++ sessionTest/sessionTest2.js
@@ -10,13 +10,10 @@ export const options = {
   iterations: 1,
 };
 
-/**
- * setup()은 테스트 수행 전 필요한 요청을 미리 수행합니다.
- * 응답으로 받은 쿠키 정보를 COOKIES 객체에 보관합니다.
- */
+// 테스트 수행 전 필요한 쿠키 정보 보관(복지관, 복지샵)
 export function setup() {
   let COOKIES = {};
-
+  let options = { cookies: COOKIES, };
   let CUSER_FORM_DATA = {
     "loginSearchBean.userId": properties.USER_ID,
     "loginSearchBean.password": encoding.b64encode(properties.PASSWORD),
@@ -25,71 +22,47 @@ export function setup() {
     "loginSearchBean.loginKindSub": "",
   };
 
-  // 복지관 로그인
-  let cuserLoginRes = http.post(properties.LOGIN_ACTION_API, CUSER_FORM_DATA, { redirects: 0 });
-  // 응답 쿠키 추출
-  for (let key in cuserLoginRes.cookies) {
-    COOKIES[key] = cuserLoginRes.cookies[key][0].value;
-  }
-
+  // 복지관 로그인 및 쿠키 보관
+  let cuserLoginRes = http.post(properties.CUSER_LOGIN_ACTION_API, CUSER_FORM_DATA, { redirects: 0 });
+  for (let key in cuserLoginRes.cookies) { COOKIES[key] = cuserLoginRes.cookies[key][0].value; }
   
-  let options = {
-    cookies: {
-      __KSMSID_USER__: COOKIES.__KSMSID_USER__,
-    },
-  };
-  // 복지샵 ASP 요청
+  // 복지샵 ASP 요청 및 Form 태그에 포함된 34개 input 태그 값 추출
   let shopAspRes = http.get(properties.SHOP_ASP_URL, options);
   let formTag = parseHTML(shopAspRes.body).find('#divLink');
-  
-  /**
-   * shopAspRes 페이지의 Form 태그에 포함된 34개 input 태그 값을 추출합니다.
-   * 추출된 form-data를 이용하여 복지샵에 loginForm.ez 요청합니다.
-   * (개선 필요 : Doc의 form을 직접 submit하는 방식으로 보다 유연한 테스트 구성)
-   */
   let SHOP_FORM_DATA = {};
-
   for (let i = 0; i < formTag.children().size(); i++) {
       let key = properties.SHOP_ASP_INPUT_NAME[i];
       let value = formTag.children().get(i).value();
       SHOP_FORM_DATA[key] = value;
   }
 
+  // 복지샵 로그인 및 쿠키 보관
   let shopLoginRes = http.post(properties.SHOP_LOGIN_ACTION_API, SHOP_FORM_DATA, { redirects: 0 });
-  // 응답 쿠키 추출
-  for (let key in shopLoginRes.cookies) {
-    COOKIES[key] = shopLoginRes.cookies[key][0].value;
-  }
-  
-  // for (let key in COOKIES) {
-  //   console.log(`${key}  -  ${COOKIES[key]}`) 
-  // }
-  
+  for (let key in shopLoginRes.cookies) { COOKIES[key] = shopLoginRes.cookies[key][0].value; }
+
   return COOKIES;
 }
 
 export default function (COOKIES) {
-
-  // 간편포인트조회 (form 데이터 및 세션 쿠키 포함 POST 요청)
-  let FORM_DATA = {
+  let options = { cookies: COOKIES, };
+  let CUSER_POINT_CALL_FORM_DATA = {
     clientCd: COOKIES.EZWEL_CLIENT_CD,
     userKey: COOKIES.EZWEL_USER_KEY,
     pageType: "BS",
     jsonType: "jsonp",
   };
 
-  let options = {
-    cookies: {
-      __KSMSID_USER__: COOKIES.__KSMSID_USER__,
-    },
-  };
-
-  let res = http.post(properties.POINT_CALL_AJAX, FORM_DATA, options);
-  console.log(res.body);
-  
-
-  check(res, { 
+  // 간편포인트조회 (form 데이터 및 세션 쿠키 포함 POST 요청) 및 검증
+  let cuserPointCallRes = http.post(properties.CUSER_POINT_CALL_AJAX, CUSER_POINT_CALL_FORM_DATA, options);
+  check(cuserPointCallRes, { 
     'HTTP status code check - 간편포인트조회 AJAX status is 200': (r) => r.status == 200, 
-    'Response body JSON data check - userName is 테크부문': (r) => JSON.parse(r.body.substring(1, res.body.length - 1)).userName == '테크부문',
+    'Response body JSON data check - userName is 테크부문': (r) => JSON.parse(r.body.substring(1, cuserPointCallRes.body.length - 1)).userName == '테크부문',
+  });
+
+  // 복지샵 메인페이지 요청 및 검증 - Response body length is 81822 and Doc title is '복지SHOP'(euc-kr)
+  let shopMainPageRes = http.get(properties.SHOP_MAIN_PAGE_URL, options);
+  check(shopMainPageRes, { 
+    'HTTP status code check - SHOP main page response status is 200': (r) => r.status == 200,
+    'Response body check - SHOP Main page loaded successfully': (r) => r.body.length > 80000 && r.html().find('title').html() == '����SHOP',
   });
 }
