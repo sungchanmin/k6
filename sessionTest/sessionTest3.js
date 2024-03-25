988980d9849755f4418220620988dcb04840712f
chanmin <adorableyou26@gmail.com>
2024-03-25 15:26:00 +0900

    🙈 스크린샷 파일 깃 허브 업로드 제외
    [docs] : .gitignore 문서 수정

--- sessionTest/sessionTest3.js
+++ sessionTest/sessionTest3.js
@@ -1,18 +1,18 @@
 import http from "k6/http";
 import encoding from "k6/encoding";
-import { browser } from 'k6/experimental/browser';
+import { browser } from "k6/experimental/browser";
 import { check } from "k6";
-import { parseHTML } from 'k6/html';
+import { parseHTML } from "k6/html";
 
-const properties = JSON.parse(open('./properties.json'));
+const properties = JSON.parse(open("./properties.json"));
 
 export const options = {
   scenarios: {
     ui: {
-      executor: 'shared-iterations',
+      executor: "shared-iterations",
       options: {
         browser: {
-          type: 'chromium',
+          type: "chromium",
         },
       },
     },
@@ -23,7 +23,7 @@ export const options = {
 export function setup() {
   const COOKIES_ARRAY = new Array();
   const COOKIES_OBJECT = {};
-  const HEADER = { cookies: COOKIES_OBJECT, };
+  const HEADER = { cookies: COOKIES_OBJECT };
   const CUSER_FORM_DATA = {
     "loginSearchBean.userId": properties.USER_ID,
     "loginSearchBean.password": encoding.b64encode(properties.PASSWORD),
@@ -33,63 +33,101 @@ export function setup() {
   };
 
   // 복지관 로그인 및 쿠키 보관
-  let cuserLoginRes = http.post(properties.CUSER_LOGIN_ACTION_API, CUSER_FORM_DATA, { redirects: 0 });
-  for (let key in cuserLoginRes.cookies) { 
+  let cuserLoginRes = http.post(
+    properties.CUSER_LOGIN_ACTION_API,
+    CUSER_FORM_DATA,
+    { redirects: 0 }
+  );
+
+  for (let key in cuserLoginRes.cookies) {
     COOKIES_OBJECT[key] = cuserLoginRes.cookies[key][0].value;
     COOKIES_ARRAY.push(cuserLoginRes.cookies[key][0]);
   }
-  
+
   // 복지샵 ASP 요청 및 Form 태그에 포함된 34개 input 태그 값 추출
   let shopAspRes = http.get(properties.SHOP_ASP_URL, HEADER);
-  let formTag = parseHTML(shopAspRes.body).find('#divLink');
+  let formTag = parseHTML(shopAspRes.body).find("#divLink");
   const SHOP_FORM_DATA = {};
-  
+
   for (let i = 0; i < formTag.children().size(); i++) {
-      let key = properties.SHOP_ASP_INPUT_NAME[i];
-      let value = formTag.children().get(i).value();
-      SHOP_FORM_DATA[key] = value;
+    let key = properties.SHOP_ASP_INPUT_NAME[i];
+    let value = formTag.children().get(i).value();
+    SHOP_FORM_DATA[key] = value;
   }
 
   // 복지샵 로그인 및 쿠키 보관
-  let shopLoginRes = http.post(properties.SHOP_LOGIN_ACTION_API, SHOP_FORM_DATA, { redirects: 0 });
-  for (let key in shopLoginRes.cookies) { 
-    COOKIES_OBJECT[key] = shopLoginRes.cookies[key][0].value; 
+  let shopLoginRes = http.post(
+    properties.SHOP_LOGIN_ACTION_API,
+    SHOP_FORM_DATA,
+    { redirects: 0 }
+  );
+
+  for (let key in shopLoginRes.cookies) {
+    COOKIES_OBJECT[key] = shopLoginRes.cookies[key][0].value;
     COOKIES_ARRAY.push(shopLoginRes.cookies[key][0]);
   }
 
-  return COOKIES_ARRAY;
+  return COOKIES_ARRAY.filter(
+    (cookie) => cookie.domain !== "" && cookie.value !== ""
+  )
+    .map(toCamel)
+    .map(domainSet);
 }
 
 export default async function (COOKIES) {
-  
-  console.log(COOKIES);
-  for(let key in COOKIES) {
-    console.log(COOKIES[key]);
-  }
-
-
   const context = browser.newContext();
   const page = context.newPage();
   context.addCookies(COOKIES);
 
   try {
     // 복지샵 페이지 요청
-    await page.goto(properties.SHOP_MAIN_PAGE_URL);
+    await page.goto(properties.SHOP_MAIN_PAGE_URL, {
+      waitUntil: "domcontentloaded",
+    });
 
-    await Promise.all([
-      page.waitForNavigation({ waitUntil: 'domcontentloaded' })
-    ]);
-
-    //check(page, { 'Main page title check --- ▒▒ 임직원을 위한 복리후생관 ▒▒': (page) => page.locator('title').textContent() == '▒▒ 임직원을 위한 복리후생관 ▒▒', });
-    page.screenshot({ path: `screenshots/MainPage_${getTimeStamp()}_${__VU}.png` });
-
+    check(page, {
+      "Main page title check --- 복지SHOP ": (page) =>
+        page.locator("title").textContent() == "복지SHOP",
+    });
+    page.screenshot({
+      path: `screenshots/ShopMainPage_${getTimeStamp()}_${__VU}.png`,
+    });
   } finally {
     page.close();
   }
 }
 
-function getTimeStamp() {
+const getTimeStamp = () => {
   let serverDate = new Date();
-  let localDate = new Date(serverDate.getTime() + (9 * 60 * 60 * 1000));
+  let localDate = new Date(serverDate.getTime() + 9 * 60 * 60 * 1000);
   return localDate.toISOString().replace(/:/g, "-");
-}
+};
+
+const toCamel = (cookie) => {
+  const newCookie = {};
+
+  for (let key in cookie) {
+    if (Object.prototype.hasOwnProperty.call(cookie, key)) {
+      let camelKey = key
+        .toLowerCase()
+        .replace(/(_\w)/g, (m) => m.toUpperCase().substr(1));
+
+      newCookie[camelKey] = cookie[key];
+    }
+  }
+
+  return newCookie;
+};
+
+const domainSet = (cookie) => {
+  const newCookie = {};
+
+  for (let key in cookie) {
+    if (Object.prototype.hasOwnProperty.call(cookie, key)) {
+      if (key == "domain") newCookie[key] = properties.DOMAIN;
+      else newCookie[key] = cookie[key];
+    }
+  }
+
+  return newCookie;
+};
